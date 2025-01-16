541a9aa51ca76fdd8fc7d1d9e66a3ae4fcab8e30
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2025-01-16 14:01:33 +0700

    Fixing Cors

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"event-management/database"
+	"event-management/middlewares"
 	"event-management/routes"
 	"event-management/seeders"
 	"event-management/structs"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,15 +24,7 @@ func main() {
 
 	router := gin.Default()
 
-	corsConfig := cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}
-
-	router.Use(cors.New(corsConfig))
+	router.Use(middlewares.CORSMiddleware())
 
 	routes.UserRoutes(router, database.DB)
 	routes.EventRoutes(router, database.DB)
