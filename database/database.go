b7d96f357028061e9cd4031452a00accdf96d9b0
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2025-01-26 21:52:46 +0700

    change some env in docker-compose

--- database/database.go
+++ database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -22,11 +21,16 @@ func Connect() {
 
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		// os.Getenv("PGHOST"),
+		// os.Getenv("PGPORT"),
+		// os.Getenv("PGUSER"),
+		// os.Getenv("PGPASSWORD"),
+		// os.Getenv("PGDATABASE"),
+		// os.Getenv("DB_HOST"),
+		// os.Getenv("DB_PORT"),
+		// os.Getenv("DB_USER"),
+		// os.Getenv("DB_PASSWORD"),
+		// os.Getenv("DB_NAME"),
 	)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
